25c11e66a7240dc2ec1cc401d4cdc12c66b261dc
Jasper Peters <jaspertjepeters@hotmail.com>
2017-06-28 20:12:13 +0200

    Framework Herkansing Versie 2
    Bug fixes:
    Hij scrolt weer gewoon naar de laatste div. Het opnieuw omhoog scrollen is ook geen probleem meer.
    
    Toevoegingen:
    Alle variabele staan nu boven in de code en zijn gemakkelijk aan te passen.
    
    Volgende toevoegingen:
    Button die je terug omhoog laat scrollen.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
 var scrollY = 0;
+var distance = 10;
+var speed = 25;
+
 
 function autoScrollTo(el) {
 	var currentY = window.pageYOffset;
 	var targetY = document.getElementById(el).offsetTop;
 	var bodyHeight = document.body.offsetHeight;
 	var yPos = currentY + window.innerHeight;
-	var animator = setTimeout('autoScrollTo(\''+el+'\')',25);
+	var animator = setTimeout('autoScrollTo(\''+el+'\')',speed);
 	if(yPos > bodyHeight){
 		clearTimeout(animator);
 	} else {
-		if(currentY < targetY-50){
-		    scrollY = currentY+50;
+		if(currentY < targetY-distance){
+		    scrollY = currentY+distance;
 		    window.scroll(0, scrollY);
 	    } else {
 		    clearTimeout(animator);
