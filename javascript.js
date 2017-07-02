ab937f51736e4f34d6ff4e4a484a7fe7fe03d78a
Jasper Peters <jaspertjepeters@hotmail.com>
2017-07-02 20:08:18 +0200

    Smooth Scroller Framework Versie 3 (Final)
    Alle variabele zijn nu niet meer globaal en kunnen dus niet meer overschreven worden door andere functies. Alle aanpassingen kunnen nu in HTML gedaan worden. Het is niet meer nodig om iets in het javascript bestand aan te passen. 
    
    Hoe gebruik je mijn framework?
    1. Download het javascript bestand en koppel deze aan je HTML.
    
    2. Voeg het volgende toe aan je <a> tag: 
        href="#" onclick="return false;" onmousedown="autoScrollTo('div1');"
        De regel hieronder laat zien wat je kunt aanpassen.
        href="#" onclick="return false;" onmousedown="autoScrollTo('id van de div waar je heen wilt scrollen');"
    
    3. Voeg de input toe voor de variabele:
       <input type="input" id="distance" value="10">
       <input type="input" id="speed" value="25">
       Deze line zorgt er voor dat je in je html de variabele aan kunt passen. Op deze manier zie je invoervelden op je pagina staan. Om deze 
       weg te halen verander je alleen het type naar "hidden". 
    
    4. Voeg de terug naar

--- javascript.js
+++ javascript.js
@@ -1,24 +1,57 @@
-var scrollY = 0;
-var distance = 10;
-var speed = 25;
-
-
 function autoScrollTo(el) {
-	var currentY = window.pageYOffset;
-	var targetY = document.getElementById(el).offsetTop;
-	var bodyHeight = document.body.offsetHeight;
-	var yPos = currentY + window.innerHeight;
-	var animator = setTimeout('autoScrollTo(\''+el+'\')',speed);
-	if(yPos > bodyHeight){
-		clearTimeout(animator);
-	} else {
-		if(currentY < targetY-distance){
-		    scrollY = currentY+distance;
-		    window.scroll(0, scrollY);
-	    } else {
-		    clearTimeout(animator);
-	    }
-	}
+    var speed = getFieldValue("speed");
+    var distance = getFieldValue("distance");
+    if(distance > 0  && speed > 0){
+        var scrollY = 0;
+	    var currentY = window.pageYOffset;
+	    var targetY = document.getElementById(el).offsetTop;
+	    var bodyHeight = document.body.offsetHeight;
+	    var yPos = currentY + window.innerHeight;
+	    var animator = setTimeout('autoScrollTo(\''+el+'\')',speed);
+        if(yPos > bodyHeight){
+            clearTimeout(animator);
+        } 
+        else {
+            if(currentY < targetY-distance){
+                scrollY = currentY+distance;
+                window.scroll(0, scrollY);
+            } else {
+                clearTimeout(animator);
+            }
+        } 
+    }
+    else{
+        alert('een van de waarder zijn geen nummers')
+    }
+}
+
+function resetScroller(el){
+    var speed = getFieldValue("speed");
+    var distance = getFieldValue("distance");
+    if(distance > 0  && speed > 0){
+        var scrollY = 0;
+        var currentY = window.pageYOffset;
+        var targetY = document.getElementById(el).offsetTop;
+        var animator = setTimeout('resetScroller(\''+el+'\')',speed);
+        if(currentY > targetY){
+            scrollY = currentY-distance;
+            window.scroll(0, scrollY);
+        } else {
+            clearTimeout(animator);
+        }
+    }
+}
+
+function getFieldValue(fieldName) {
+    var result = 0;
+   var field = document.getElementById(fieldName);
+    if(field && isNumeric(field.value) ){
+        result = parseInt(field.value);
+    }
+    return result;
 }
 
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
